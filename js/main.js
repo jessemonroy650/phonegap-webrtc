3f628a7e11d0c297867f9d3753d93c2aafae8591
Jesse Monroy <jessemonroy650@yahoo.com>
2015-06-26 01:02:59 -0700

    trying permissions for camera

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   // PeerJS server location
-  var SERVER_IP = '192.168.0.25';
+  var SERVER_IP = '192.168.1.180';
   var SERVER_PORT = 9000;
 
   // DOM elements manipulated as user interacts with the app
